075c8c692a73daed08590d2b14e22a5e4310ad2c
QuarkPixel <m2462864403@gmail.com>
2022-03-20 12:09:40 +0800

    新增捐赠表数据

--- data/sponsorList.js
+++ data/sponsorList.js
@@ -1,8 +1,19 @@
 var sponsorListII = [
-    "1uckily$62.8",
-    ""
+    "sid_T$12.00",
+    "QuarkPixel$33.09",
+    "1uckily$62.80",
 ];
 
 var sponsorListI = [
-
+    "zamonas$120.00",
+    "Aiermisa$100.00",
+    "dream_station$180.00",
+    "Dream boy$11.11",
+    "55kaikaikai$8.89",
+    "jackmsq$300.00",
+    "Sun_miss$6.66",
+    "SYQ$1.11",
+    "zero_8_5$50.00",
+    "uncle_hua$30.00",
+    "kami$20.00",
 ]
