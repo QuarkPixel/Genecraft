b16dddf8c29bdf334d20485f2f6c04828a82e1a0
QuarkPixel <m2462864403@gmail.com>
2022-03-20 10:39:32 +0800

    更改div文字

--- surprise/sprz.js
+++ surprise/sprz.js
@@ -4,7 +4,7 @@ xmlhttp.open('GET', link, false)
 xmlhttp.send()
 if (xmlhttp.status == 200) {
     link = '<img src="' + link + '">';
-    $(".message").prepend('<div>恭喜你 发现了编号为' + link + '的彩蛋</div>');
+    $(".message").prepend('<div>恭喜你 发现了我服编号为' + link + '的彩蛋</div>');
     $(".backGroundUP").append(link);
     $(".backGroundDown").append(link);
 } else {
