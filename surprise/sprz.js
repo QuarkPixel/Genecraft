ac2efbd5be2c7ca86d9fa9c032a03f1766ba1bf2
QuarkPixel <m2462864403@gmail.com>
2022-03-24 11:41:54 +0800

    格式化

--- surprise/sprz.js
+++ surprise/sprz.js
@@ -1,21 +1,23 @@
 var link =
-    "../resources/surpriseSvg/" + window.location.hash.replace("#", "") + ".svg"
-var xmlhttp = new XMLHttpRequest()
-xmlhttp.open("GET", link, false)
-xmlhttp.send()
+    "../resources/surpriseSvg/" +
+    window.location.hash.replace("#", "") +
+    ".svg";
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", link, false);
+xmlhttp.send();
 if (xmlhttp.status == 200) {
-    link = '<img src="' + link + '">'
+    link = '<img src="' + link + '">';
     $(".message").prepend(
         "<div>恭喜你 发现了我服编号为" + link + "的彩蛋</div>"
-    )
-    $(".backGroundUP").append(link)
-    $(".backGroundDown").append(link)
+    );
+    $(".backGroundUP").append(link);
+    $(".backGroundDown").append(link);
 } else {
-    window.location.href = "../404.html"
+    window.location.href = "../404.html";
 }
 
 function time() {
-    var date = new Date()
+    var date = new Date();
     document.getElementById("time").innerHTML =
         date.getFullYear().toString() +
         "." +
@@ -27,9 +29,9 @@ function time() {
         ":" +
         PrefixZero(date.getMinutes().toString()) +
         ":" +
-        PrefixZero(date.getSeconds().toString())
+        PrefixZero(date.getSeconds().toString());
 }
 function PrefixZero(num) {
-    return (Array(2).join(0) + num).slice(-2)
+    return (Array(2).join(0) + num).slice(-2);
 }
-setInterval("time()", 1000)
+setInterval("time()", 1000);
