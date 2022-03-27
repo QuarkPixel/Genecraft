67223654170b3daa39bb2f849c9c7eae2c31e1cc
QuarkPixel <m2462864403@gmail.com>
2022-03-27 19:34:10 +0800

    将 公告 黑名单 历史 赞助表集合为一个main.js文件

---
+++ data/main.js
@@ -0,0 +1,65 @@
+//!食用方法见主文件夹内的 Readme.md!
+
+////AnnouncementText
+//First
+var announcementTitle_I = "我们加入爱发电啦！";
+var announcementBody_I =
+    '<a class="href" href="https://afdian.net/@genesis_craft">*戳我前往爱发电主页查看</a>';
+var displayTimeEnd_I = "20220210$20220510";
+
+//Second
+var announcementTitle_II = "图片投稿";
+var announcementBody_II =
+    "官网重新装修，需要多张照片。大佬们可以投稿你们的建筑，或者好看的景观。截图即可，像素高清点。<br />上传至「群相册-景观收集」即可";
+var displayTimeEnd_II = "$";
+
+//Last
+var announcement_X =
+    '暂时没有公告，可以去看看网页作者的<a class="href" href="https://twitter.com/Quark_Pixel">Twitter</a>？';
+
+var sponsorListII = [
+    "QuarkPixel$10.00",
+    "方块国度$10.00",
+    "sid_T$12.00",
+    "QuarkPixel$33.09",
+    "1uckily$62.80",
+];
+
+////SponsorList
+var sponsorListI = [
+    "zamonas$120.00",
+    "Aiermisa$100.00",
+    "dream_station$180.00",
+    "Dream boy$11.11",
+    "55kaikaikai$8.89",
+    "Jackmsq$300.00",
+    "Sun_miss$6.66",
+    "SYQ$1.11",
+    "zero_8_5$50.00",
+    "uncle_hua$30.00",
+    "kami$20.00",
+];
+
+////BannedList
+var bannedList = [
+    "TameableEmu1974$使用TNT破坏出生点装饰$反复退群 干扰管理秩序$2021.11.17$",
+    "Xx_wsx666_xX$多次使用作弊$破坏玩家机器$2021.11.14$QQ:17204***64",
+    "LoveMin$大量白嫖 抄家$$2021.10.16$",
+    "chinanb$随意拆东西 且赔偿不积极$恶意熊服 破坏机器$2021.9.19$",
+    "xxii222$破坏多个机器$过度白嫖$2021.9.19$",
+    "luyifan351$随意动机器导致其损毁$$2021.9.4$",
+    "winnt351$矿透$$2021.7.21$",
+    "ExpertAir135653$两次过度白嫖$$2021.7.23$",
+    "ljh$熊服$$2021.7.17$",
+    "TWK_laochen$公然开挂$$2021.10.1$QQ:7254***85",
+    "fan0619$熊服$矿透$2021.6.30$",
+];
+
+////HistoryEvent
+var historyList = [
+    "2021.10.7$新官网开始搭建$由 QuarkPixel 一人编写",
+    "2021.6.10$BE端支持加入服务器$打破次元壁",
+    "2021.5.27$服务器对外开放$欢迎所有人加入",
+    "2021.5.10$服务器调试完毕$开始小范围试点",
+    "2021.5.9$Genesis Craft建立$聊天群创建",
+];
