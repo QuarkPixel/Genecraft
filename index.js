f2bce910253c2c74e13429f87d562dcfae7101ac
QuarkPixel <m2462864403@gmail.com>
2022-06-25 22:50:28 +0800

    格式化代码|开启检测页面刷新

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //Cover;
 if (window.name == "") {
-    $(".blackCloth").css("background-color", "var(--black10)");
-    $(".page_welcome").addClass("page_welcome_animation");
+    $(".blackCloth").css("background-color", "var(--black10)")
+    $(".page_welcome").addClass("page_welcome_animation")
     setTimeout(function () {
         $(".blackCloth").css({
             "background-color": "var(--black0)",
             transition: "ease-in 2.5s",
-        });
-    }, 100);
-    window.name = "isReload";
+        })
+    }, 100)
+    window.name = "isReload"
 }
 
 //Create many dividers
@@ -17,7 +17,7 @@ for (let i = 0; i < Math.ceil(window.innerWidth / 480); i++) {
         '<img style="--i: ' +
             Math.round(Math.random()) +
             '" src="https://imgtu.ly2018.top/img/2022/06/220620_fAgJfuvd_divider.png">'
-    );
+    )
 }
 
 //Announcement
@@ -25,66 +25,66 @@ dateNow = parseInt(
     new Date().getYear().toString().substr(1) +
         PrefixZero((new Date().getMonth() + 1).toString()) +
         PrefixZero(new Date().getDate().toString())
-);
+)
 
 for (let i = 0; i < announcementText.length; i++) {
-    var announcementInfo = announcementText[i].split("$");
-    var displayTimeEnd = announcementInfo[2].split("-");
+    var announcementInfo = announcementText[i].split("$")
+    var displayTimeEnd = announcementInfo[2].split("-")
 
     if (
         dateNow < parseInt(displayTimeEnd[0]) ||
         dateNow > parseInt(displayTimeEnd[1])
     ) {
         if (i == announcementText.length - 1) {
-            $(".announcement").remove();
+            $(".announcement").remove()
         }
     } else {
         $(".announcementBody").append(
             '<p id="announcement-title">' + announcementInfo[0] + "</p>"
-        );
+        )
         $(".announcementBody").append(
             '<p id="announcement-body">' + announcementInfo[1] + "</p>"
-        );
-        break;
+        )
+        break
     }
 }
 
 function PrefixZero(num) {
-    return (Array(2).join(0) + num).slice(-2);
+    return (Array(2).join(0) + num).slice(-2)
 }
 
 //Bullet
-createBullet_MaxTime = historyList.length - 1;
-createBullet_Times = createBullet_MaxTime;
-var boxH = $(".bulletBox").height();
-var numberRows = parseInt(boxH / 64);
+createBullet_MaxTime = historyList.length - 1
+createBullet_Times = createBullet_MaxTime
+var boxH = $(".bulletBox").height()
+var numberRows = parseInt(boxH / 64)
 
 function randomArrayFunction() {
     randomArray = rowPosition.sort(function () {
-        return 0.5 - Math.random();
-    });
+        return 0.5 - Math.random()
+    })
 }
 
 if (numberRows != 0) {
-    var interval = Math.round(20000 / (numberRows + 0.6));
-    var rowClearance = (boxH - 50 * numberRows) / (2 * numberRows);
-    var rowPosition = new Array();
+    var interval = Math.round(20000 / (numberRows + 0.6))
+    var rowClearance = (boxH - 50 * numberRows) / (2 * numberRows)
+    var rowPosition = new Array()
     for (i = 0; i < numberRows; i++) {
         rowPosition.push(
             (2 * i * rowClearance + rowClearance + 50 * i).toFixed()
-        );
+        )
     }
-    randomArray_Times = 0;
-    randomArrayFunction();
+    randomArray_Times = 0
+    randomArrayFunction()
 
     setInterval(function () {
-        var pageW = $(".bulletBox").width() + "px";
-        var listOne = historyList[createBullet_Times].split("$");
-        var date = listOne[0].split(".");
+        var pageW = $(".bulletBox").width() + "px"
+        var listOne = historyList[createBullet_Times].split("$")
+        var date = listOne[0].split(".")
         if (Math.random() * 10 < 0.05) {
             var newText = $(
                 '<a href="/surprise/index.html#hgg1mlsdlpiy" target="_blank">真的有人会看这个嘛 awa</a>'
-            );
+            )
         } else {
             var newText = $(
                 '<a href="history" ><i class="time"><i class="year">' +
@@ -98,12 +98,12 @@ if (numberRows != 0) {
                     " " +
                     listOne[2] +
                     "</a>"
-            );
+            )
         }
         newText.css({
             left: pageW,
             top: randomArray[randomArray_Times] + "px",
-        });
+        })
         newText.animate(
             {
                 left: -$(this).width(),
@@ -111,33 +111,33 @@ if (numberRows != 0) {
             15000,
             "linear",
             function () {
-                $(this).remove();
+                $(this).remove()
             }
-        );
-        newText.appendTo(".bulletText");
+        )
+        newText.appendTo(".bulletText")
         if (createBullet_Times <= 0) {
-            createBullet_Times = createBullet_MaxTime;
+            createBullet_Times = createBullet_MaxTime
         } else {
-            createBullet_Times -= 1;
+            createBullet_Times -= 1
         }
         if (randomArray_Times >= numberRows - 1) {
-            randomArray_Times = 0;
-            randomArrayLastTime = randomArray[numberRows - 1];
+            randomArray_Times = 0
+            randomArrayLastTime = randomArray[numberRows - 1]
             do {
-                randomArrayFunction();
-            } while (randomArrayLastTime == randomArray[0]);
+                randomArrayFunction()
+            } while (randomArrayLastTime == randomArray[0])
         } else {
-            randomArray_Times++;
+            randomArray_Times++
         }
-    }, interval);
+    }, interval)
 } else {
-    $(".bulletBox").css("display", "none");
+    $(".bulletBox").css("display", "none")
 }
 
 //List
-var sponsorListLoadingTimes = 0;
-createFramework_sponsorList(sponsorListII);
-createFramework_sponsorList(sponsorListI);
+var sponsorListLoadingTimes = 0
+createFramework_sponsorList(sponsorListII)
+createFramework_sponsorList(sponsorListI)
 
 function createFramework_sponsorList(array) {
     for (
@@ -145,48 +145,48 @@ function createFramework_sponsorList(array) {
         sponsorListLoadingTimes < array.length && sponsorListLoadingTimes < 9;
         sponsorListLoadingTimes++
     ) {
-        var listOne = array[sponsorListLoadingTimes].split("$");
+        var listOne = array[sponsorListLoadingTimes].split("$")
         $(".sLIndex").append(
             '<tr><td class="playerID" lang="en">' +
                 listOne[0] +
                 "</td><td>￥" +
                 listOne[1] +
                 "</td></tr>"
-        );
+        )
     }
 }
 
 //Create Framework
 
-var previewPicture = document.querySelector(".previewPicture");
-var previewParent = document.querySelector(".item");
+var previewPicture = document.querySelector(".previewPicture")
+var previewParent = document.querySelector(".item")
 
 var randomArrayImages = [0, 1, 2, 3, 4, 5, 6].sort(function () {
-    return Math.random() > 0.5 ? -1 : 1;
-});
+    return Math.random() > 0.5 ? -1 : 1
+})
 
 for (let i = 0; i < 7; i++) {
     $(".item").append(
         '<div><a href="gallery" class="imgA"><img src="' +
             imgHref[randomArrayImages[i]].replace("&r;", abbreviations) +
             '"></a></div>'
-    );
+    )
 }
 
 // Zoom
-previewA = document.querySelector(".imgA");
-imgBorderHeight = previewA.offsetHeight + 1 + "px";
-imgBorderWidth = previewA.offsetWidth + 1 + "px";
+previewA = document.querySelector(".imgA")
+imgBorderHeight = previewA.offsetHeight + 1 + "px"
+imgBorderWidth = previewA.offsetWidth + 1 + "px"
 window.onload = function () {
-    var item = document.querySelectorAll(".imgA img");
+    var item = document.querySelectorAll(".imgA img")
     for (let i = 0; i < item.length; i++) {
         if (0.6 > item[i].height / item[i].width) {
-            item[i].style.height = imgBorderHeight;
+            item[i].style.height = imgBorderHeight
         } else {
-            item[i].style.width = imgBorderWidth;
+            item[i].style.width = imgBorderWidth
         }
     }
-};
+}
 
 //Loadover
 $(document).ready(function () {
@@ -199,7 +199,7 @@ $(document).ready(function () {
             '<svg class="flipArrow" width="25px" height="25px" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path fill="#1d1d1f" d="M635.733333 512l-149.333333-149.333333L426.666667 422.4l89.6 89.6-89.6 89.6 59.733333 59.733333 149.333333-149.333333zM170.666667 512c0 187.733333 153.6 341.333333 341.333333 341.333333s341.333333-153.6 341.333333-341.333333-153.6-341.333333-341.333333-341.333333-341.333333 153.6-341.333333 341.333333z m85.333333 0c0-140.8 115.2-256 256-256s256 115.2 256 256-115.2 256-256 256-256-115.2-256-256z"  /></svg>',
         dots: true,
         appendDots: $(".dotsDiv"),
-    });
+    })
 
     $(".item").slick({
         accessibility: false,
@@ -213,12 +213,12 @@ $(document).ready(function () {
         variableWidth: true,
         centerMode: true,
         pauseOnHover: false,
-    });
-});
+    })
+})
 
-// var refreshWithDetectionWidth = $(window).width();
-// setInterval(function () {
-//     if ($(window).width() != refreshWithDetectionWidth) {
-//         location.reload();
-//     }
-// }, 5000);
+var refreshWithDetectionWidth = $(window).width()
+setInterval(function () {
+    if ($(window).width() != refreshWithDetectionWidth) {
+        location.reload()
+    }
+}, 5000)
